1ed954f259e9d6295fbdbd9b374875bf7f68d0aa
DamifeZion <damifemartins@gmail.com>
2024-09-08 22:52:59 +0100

    feat: Update the our team section

--- src/components/home/team-card.tsx
+++ src/components/home/team-card.tsx
@@ -1,19 +1,13 @@
-import { CiLinkedin } from "react-icons/ci";
-import { CiTwitter } from "react-icons/ci";
-import { CiMail } from "react-icons/ci";
-import { Button } from "../ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card"
 
 export type TeamCardProps = React.HTMLAttributes<HTMLDivElement> & {
    src: string;
    name: string;
    position: string;
-   email: string;
-   twitterLink?: string;
-   linkedInLink?: string;
+   description: string;
 }
 
-export const TeamCard: React.FC<TeamCardProps> = ({ src, name, position, email, twitterLink, linkedInLink }) => {
+export const TeamCard: React.FC<TeamCardProps> = ({ src, name, position, description }) => {
 
    return (
       <Card className="border shadow-none bg-[#F2F2F2]">
@@ -25,32 +19,13 @@ export const TeamCard: React.FC<TeamCardProps> = ({ src, name, position, email, 
             />
 
             <CardTitle className="text-xl leading-none !mt-4">
-               {name}
+               {name} <br /> <span className="text-sm md:text-md">({position})</span>
             </CardTitle>
+
             <CardDescription>
-               {position}
+               {description}
             </CardDescription>
          </CardHeader>
-
-         <CardContent className="pt-14 flex flex-wrap items-center justify-center gap-4 *:w-fit [&_button]:bg-primary/10 [&_button:hover]:bg-primary/15 [&_button:hover]:scale-105 [&_button]:size-fit [&_button]:p-1.5 [&_button]:rounded-full [&_svg]:size-6 [&_svg]:text-primary">
-            <a href={`mailto:${email}`}>
-               <Button size="icon" >
-                  <CiMail />
-               </Button>
-            </a>
-
-            <a href={twitterLink} target="_blank">
-               <Button size="icon" >
-                  <CiTwitter />
-               </Button>
-            </a>
-
-            <a href={linkedInLink} target="_blank">
-               <Button size="icon" >
-                  <CiLinkedin />
-               </Button>
-            </a>
-         </CardContent>
       </Card>
    )
 }
